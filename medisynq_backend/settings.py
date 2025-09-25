cf6c858d07213c582ffad571abb27a7689fbc136
Stepstar <stephenodong849@gmail.com>
2025-09-26 02:50:38 +0300

    These changes are staged for commit.

--- medisynq_backend/settings.py
+++ medisynq_backend/settings.py
@@ -98,6 +98,6 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
-    "https://your-username.github.io",  # replace with your actual GitHub Pages domain
+    "https://stephstar.github.io/Medisynq/",  # replace with your actual GitHub Pages domain
     "https://medisynq-1.onrender.com",  # your backend domain
 ]
